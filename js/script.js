a1ec9d4c4d8d1711df67aa83156570564a7a16f2
alberto serrano <albert.serrano.ja@gmail.com>
2017-11-09 03:48:18 -0800

    feat: Include a second cat into the site

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-var count = 0
+var count1 = 0
+var count2 = 0
 
-$('#cat-pic').click(function(e) {
-  count += 1;
-  document.getElementById('counter').innerHTML = count;
+var name1 = "Bob";
+var name2 = "Dylan";
+
+document.getElementById('name1').innerHTML = name1;
+document.getElementById('name2').innerHTML = name2;
+
+$('#cat-pic1').click(function(e) {
+  count1 += 1;
+  document.getElementById('counter1').innerHTML = count1;
+});
+
+$('#cat-pic2').click(function(e) {
+  count2 += 1;
+  document.getElementById('counter2').innerHTML = count2;
 });
