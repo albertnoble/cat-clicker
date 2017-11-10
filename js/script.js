df188e3eaafc1650af15fdbc83b50e731f97ac99
alberto serrano <albert.serrano.ja@gmail.com>
2017-11-10 14:59:45 -0800

    feat: Integrate a list of cats on the sidebar

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ var name2 = "Dylan";
 document.getElementById('name1').innerHTML = name1;
 document.getElementById('name2').innerHTML = name2;
 
+
 $('#cat-pic1').click(function(e) {
   count1 += 1;
   document.getElementById('counter1').innerHTML = count1;
