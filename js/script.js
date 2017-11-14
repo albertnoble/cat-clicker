7caa5d360c2ddbc44e6dbb717e34e71954b45756
alberto serrano <albert.serrano.ja@gmail.com>
2017-11-14 03:50:49 -0800

    feat: Implement MVO structure with cat clicker

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 
 var model = {
+    currentCat: null,
     cats: [
         {
             count: 0,
@@ -25,31 +26,92 @@ var model = {
             count: 0,
             name: "ZebroWiskey",
             imgUrl: "images/cat5.jpg"
-        }]
+        }
+    ]
 };
 
-//initial cat
-document.getElementById('cat-pic').src = model.cats[0].imgUrl;
-document.getElementById('name').innerHTML = model.cats[0].name;
-document.getElementById('counter').innerHTML = model.cats[0].count;
-
-
-var catList = document.getElementsByClassName("thumbnail");
-
-var currentCat = 0;
-for (let i = 0; i < catList.length; i++) {
-    var cat = catList[i];
-    cat.addEventListener('click', function() {
-        document.getElementById('cat-pic').src = model.cats[i].imgUrl;
-        document.getElementById('name').innerHTML = model.cats[i].name;
-        document.getElementById('counter').innerHTML = model.cats[i].count;
-        currentCat = i;
+var octopus = {
+    init: function(){
+        model.currentCat = model.cats[0];
         
-    });   
+        catListView.init();
+        catView.init();
+    },
     
-}
+    getCurrentCat: function(){
+        return model.currentCat;
+    },
+    
+    getCats: function(){
+        return model.cats;
+    },
+    
+    setCurrentCat: function(cat){
+        model.currentCat = cat;
+    },
+    
+    incrementCounter: function(){
+        model.currentCat.count++;
+        catView.render();
+    }
+    
+};
 
-$('#cat-pic').click(function(e) {
-        model.cats[currentCat].count += 1;
-        document.getElementById('counter').innerHTML = model.cats[currentCat].count;
-});
+var catView = {
+
+    init: function() {
+        
+        this.catNameElem = document.getElementById('name');
+        this.catImageElem = document.getElementById('cat-pic');
+        this.countElem = document.getElementById('counter');
+
+        this.catImageElem.addEventListener('click', function(){
+            octopus.incrementCounter();
+        });
+
+        this.render();
+    },
+
+    render: function() {
+        var currentCat = octopus.getCurrentCat();
+        this.countElem.textContent = currentCat.count;
+        this.catNameElem.textContent = currentCat.name;
+        this.catImageElem.src = currentCat.imgUrl;
+    }
+};
+
+var catListView = {
+
+    init: function() {
+        this.catListElem = document.getElementById('catlist');
+        this.render();
+    },
+
+    render: function() {
+        var cat, elem, i;
+        var cats = octopus.getCats();
+
+        this.catListElem.innerHTML = '';
+
+        for (i = 0; i < cats.length; i++) {
+            cat = cats[i];
+
+            elem = document.createElement('li');
+            pic = document.createElement('img');
+            pic.src = cat.imgUrl;
+            pic.classList.add('smallpic')
+            elem.appendChild(pic);
+
+            elem.addEventListener('click', (function(catCopy) {
+                return function() {
+                    octopus.setCurrentCat(catCopy);
+                    catView.render();
+                };
+            })(cat));
+
+            this.catListElem.appendChild(elem);
+        }
+    }
+};
+
+octopus.init();
